6d8b3406bac2c49c2d2cae0178eaf8fbdb131ed5
KartikSGulkotwar <114782932+KartikSGulkotwar@users.noreply.github.com>
2024-02-05 03:59:55 +0000

    commit 3

--- Lab4SP/Lab4.c
+++ Lab4SP/Lab4.c
@@ -1,56 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void concatenateFiles(char *filename1, char *filename2) {
-    FILE *file1, *file2;
+void concat_Files(char *filename1, char *filename2) {
+    FILE *file_1, *file_2;
     char ch;
 
-    // Open the first file for reading
-    file1 = fopen(filename1, "a");
+    // Opening the first file
+    file_1 = fopen(filename1, "a");
 
-    if (file1 == NULL) {
-        perror("Error opening first file");
+    if (file_1 == NULL) {
+        perror("Error while opening the first file");
         exit(EXIT_FAILURE);
     }
 
-    // Open the second file for reading
-    file2 = fopen(filename2, "r");
+    // Opening the second file
+    file_2 = fopen(filename2, "r");
 
-    if (file2 == NULL) {
-        perror("Error opening second file");
-        fclose(file1);
+    if (file_2 == NULL) {
+        perror("Error while opening the second file");
+        fclose(file_1);
         exit(EXIT_FAILURE);
     }
 
-    // Check if filenames are the same
+    // Checking if the file names are the same
     if (strcmp(filename1, filename2) == 0) {
-        fprintf(stderr, "Error: The two filenames are the same.\n");
-        fclose(file1);
-        fclose(file2);
+        fprintf(stderr, "Error:Two filenames are same.\n");
+        fclose(file_1);
+        fclose(file_2);
         exit(EXIT_FAILURE);
     }
 
-    // Copy the contents of the second file to the first file
-    while ((ch = fgetc(file2)) != EOF) {
-        fputc(ch, file1);
+    // Copying the contents of the second file to the first file.
+    while ((ch = fgetc(file_2)) != EOF) {
+        fputc(ch, file_1);
     }
 
-    // Close the files
-    fclose(file1);
-    fclose(file2);
+    // To Close the files
+    fclose(file_1);
+    fclose(file_2);
 }
 
 int main(int argc, char *argv[]) {
-    // Check if two filenames are provided as command-line arguments
+    // to Check if two filenames are provided as command-line arguments
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <filename1> <filename2>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Concatenate the contents of the second file to the first file
-    concatenateFiles(argv[1], argv[2]);
+    // Concatenating the content of the second file to the first file
+    concat_Files(argv[1], argv[2]);
 
-    printf("Files concatenated successfully.\n");
+    printf("Files is concatenated successfully.\n");
 
     return 0;
 }
